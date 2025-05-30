14c174772dc79c521159e6dd2006e419026d867a
Ricardo Garza <rgarza0416@yahoo.com>
2025-05-29 23:04:48 -0500

    Initial Commit

---
+++ TakeHomeProjectFetch/TakeHomeProjectFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeProjectFetchApp.swift
+//  TakeHomeProjectFetch
+//
+//  Created by Ricardo Garza on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeProjectFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
